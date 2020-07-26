1cc6267f99f7d6cd780a7ad146139e7aee5229e7
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-07-26 14:54:56 -0400

    feat: Add AHMA brand typefaces from Adobe Fonts

--- web/app/themes/abundanthousingma/resources/views/partials/head.blade.php
+++ web/app/themes/abundanthousingma/resources/views/partials/head.blade.php
@@ -2,5 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://use.typekit.net/ydj6ppp.css">
   @php wp_head() @endphp
 </head>
