2767c58c48511eb3877a3437d78bdcf0b1d578bc
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-07-26 21:41:10 -0400

    feat: Lay out & style global header
    * Add site logo
    * Set up three-part nav: member login, social, site nav
    * Custom styles for “Donate” custom menu item as a faux-button

--- web/app/themes/abundanthousingma/resources/views/partials/header.blade.php
+++ web/app/themes/abundanthousingma/resources/views/partials/header.blade.php
@@ -1,10 +1,19 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-    <nav class="nav-primary">
-      @if (has_nav_menu('primary_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
-      @endif
-    </nav>
-  </div>
+  <a class="brand" href="{{ home_url('/') }}"><img src="/app/themes/abundanthousingma/resources/assets/images/ahma-logo.svg" alt="Abundant Housing MA logo" title="{{ get_bloginfo('name', 'display') }}"></a>
+
+  <nav class="nav-primary">
+    <div class="nav-primary-extras">
+      <a href="/login" title="Member Login">Member Login</a>
+
+      <div class="header-social">
+        <a href="https://twitter.com/AbundantHomesMA"><img src="/app/themes/abundanthousingma/resources/assets/images/header-social-twitter.svg" alt="Twitter profile"></a>
+        <a href="https://www.facebook.com/AbundantHousingMA"><img src="/app/themes/abundanthousingma/resources/assets/images/header-social-facebook.svg" alt="Facebook Page"></a>
+        <a href="https://www.linkedin.com/company/abundant-housing-ma/"><img src="/app/themes/abundanthousingma/resources/assets/images/header-social-linkedin.svg" alt="LinkedIn company profile"></a>
+      </div>
+    </div>
+
+    @if (has_nav_menu('primary_navigation'))
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
+    @endif
+  </nav>
 </header>
