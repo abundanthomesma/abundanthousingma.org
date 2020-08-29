d6f4ff9d8810c9635fde1cbb4a997c142439d527
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-08-29 10:52:37 -0400

    fix: Update footer copy per non-profit status
    AHMA isn’t yet a true 501(c)(4), so we cannot claim that.

--- site/web/app/themes/abundanthousingma/resources/views/partials/footer.blade.php
+++ site/web/app/themes/abundanthousingma/resources/views/partials/footer.blade.php
@@ -1,10 +1,12 @@
 <footer class="footer-site">
   <div class="wp-block-group__inner-container">
     <div class="footer-copyright">
-      <p><span class="footer-copyright-name">Abundant Housing Massachusetts</span><br>
+      {{-- <p><span class="footer-copyright-name">Abundant Housing Massachusetts</span><br>
         Copyright © 2020 AHMA Action, a 501(c)(4) organization.<br>
-        Contributions are not tax-exempt.
-      </p>
+        Contributions are not tax-exempt.</p> --}}
+      <p><span class="footer-copyright-name">Abundant Housing Massachusetts</span><br>
+        Copyright © 2020<br>
+        AHMA is a Massachusetts not-for-profit corporation</p>
     </div>
 
     <div class="footer-signup">
