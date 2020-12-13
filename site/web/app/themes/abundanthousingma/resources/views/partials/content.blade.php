0cba97afa30af138edd2e8e3f148e9e1a8ad1ff5
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-12-13 14:28:56 -0500

    fix: Display entire post content on indexes

--- site/web/app/themes/abundanthousingma/resources/views/partials/content.blade.php
+++ site/web/app/themes/abundanthousingma/resources/views/partials/content.blade.php
@@ -3,7 +3,10 @@
     <h2 class="entry-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h2>
     @include('partials/entry-meta')
   </header>
+
   <div class="entry-summary">
-    @php the_excerpt() @endphp
+    @php the_content() @endphp
   </div>
+
+  <hr>
 </article>
