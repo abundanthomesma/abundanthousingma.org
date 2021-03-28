91cf89bf94e501b564078f8c3fcad82926e78916
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-03-28 15:37:19 -0400

    feat: Add Page title to header-less pages
    Breaks things up a bit.

--- site/web/app/themes/abundanthousingma/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/abundanthousingma/resources/views/partials/content-page.blade.php
@@ -1,2 +1,3 @@
+<h1 class="page-title"><a href="{{ get_permalink() }}">{!! get_the_title() !!}</a></h1>
 @php the_content() @endphp
 {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
