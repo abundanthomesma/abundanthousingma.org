868b2c5f272724d7f6d8773bd4611bebc4c71b0b
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-06-03 16:08:26 -0400

    Switch to stylelint-config-recommended-scss
    Allows us to properly accommodate SCSS.
    
    Details:
    github.com/stylelint/stylelint/issues/5289#issuecomment-840772900

--- site/web/app/themes/abundanthousingma/.stylelintrc.js
+++ site/web/app/themes/abundanthousingma/.stylelintrc.js
@@ -1,35 +1,13 @@
 module.exports = {
-  'extends': 'stylelint-config-standard',
+  'extends': 'stylelint-config-recommended-scss',
+  'plugins': [
+    'stylelint-scss',
+  ],
   'rules': {
     'no-empty-source': null,
     'string-quotes': 'double',
     'at-rule-empty-line-before': null,
-    'at-rule-no-unknown': [
-      true,
-      {
-        'ignoreAtRules': [
-          'extend',
-          'at-root',
-          'debug',
-          'warn',
-          'error',
-          'if',
-          'else',
-          'for',
-          'each',
-          'while',
-          'mixin',
-          'include',
-          'content',
-          'return',
-          'function',
-          'tailwind',
-          'apply',
-          'responsive',
-          'variants',
-          'screen',
-        ],
-      },
-    ],
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
   },
 };
