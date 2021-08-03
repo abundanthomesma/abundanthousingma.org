45fc7020c3e2999cc404d0dbcfb3b1da9c31fb90
Jeff Byrnes <thejeffbyrnes@gmail.com>
2021-08-03 18:59:18 -0400

    Avoid widow w/ header signup text

--- site/web/app/themes/abundanthousingma/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/abundanthousingma/resources/views/partials/page-header.blade.php
@@ -1,6 +1,6 @@
 <header class="page-header wp-block-group">
   <div class="wp-block-group__inner-container">
-    <h2 class="page-header__headline">Together, we can make Massachusetts for everyone</h2>
+    <h2 class="page-header__headline">Together, we can make Massachusetts for&nbsp;everyone</h2>
 
     <div class="header-signup">
       <script src="https://actionnetwork.org/widgets/v4/form/join-our-mailing-list-82?format=js&source=widget&referrer=group-abundant-housing-ma"></script>
